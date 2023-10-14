c6bba5db810c73fb92198411af9988f45504aea3
Salandora <salandora@gmail.com>
2023-10-14 18:00:13 +0200

    Forgot tzo add purpur_plugin earlier

---
+++ purpur_plugin/src/main/java/com/github/salandora/rideableravagers/RideableRavagersPlugin.java
@@ -0,0 +1,13 @@
+package com.github.salandora.rideableravagers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.jetbrains.annotations.NotNull;
+
+@DefaultQualifier(NotNull.class)
+public final class RideableRavagersPlugin extends JavaPlugin {
+	@Override
+	public void onEnable() {
+	}
+
+}
