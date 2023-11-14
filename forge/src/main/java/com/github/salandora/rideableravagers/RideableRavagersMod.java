66f478fb7d8763e5ec68cde3cafd0f15644c9c02
Salandora <salandora@gmail.com>
2023-11-14 11:04:11 +0100

    * Fix a bug that made forge servers crash    * Due to the bug partially being in Midnightlib it was necessary to switch to a forked version    * This bug only affected RideableRavagers for Minecraft 1.20.1

--- forge/src/main/java/com/github/salandora/rideableravagers/RideableRavagersMod.java
+++ forge/src/main/java/com/github/salandora/rideableravagers/RideableRavagersMod.java
@@ -3,11 +3,9 @@ package com.github.salandora.rideableravagers;
 import com.github.salandora.rideableravagers.config.RideableRavagersConfig;
 import com.github.salandora.rideableravagers.init.BiomeCodecInit;
 import eu.midnightdust.lib.config.MidnightConfig;
-import net.minecraftforge.client.ConfigScreenHandler;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -17,9 +15,6 @@ public class RideableRavagersMod {
 
 	public RideableRavagersMod() {
 		MidnightConfig.init(MODID, RideableRavagersConfig.class);
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "SERVER_ONLY", (remote, server) -> true));
-		ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
-				new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> MidnightConfig.getScreen(parent, MODID)));
 
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
